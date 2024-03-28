760eddea9430a101fc08966a284d9491cce89988
Alekseymedvedev <61234001+Alekseymedvedev@users.noreply.github.com>
2024-03-28 16:27:01 +0300

    Delete client directory

--- client/routes/routes.ts
+++
@@ -1,8 +0,0 @@
-
-
-export const routesPage = [
-    {path: "/", name: "Главная"},
-    {path: "/contacts", name: "Контакты"},
-    {path: "/categories", name: "Услуги"},
-    {path: "/about", name: "О нас"},
-]
